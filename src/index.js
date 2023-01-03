0521ba51d64053c5b4f33bd99f5f8526ea5cd1f6
NerdyTechy <techy@techy.lol>
2023-01-03 11:13:52 +0000

    Formatting with prettier

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-const db = require('./utils/database');
+const db = require("./utils/database");
 db.init();
 
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-app.get('/:profile', async (req, res) => {
+app.get("/:profile", async (req, res) => {
     const { profile } = req.params;
     const { style, color } = req.query;
     var views = (await db.query(`SELECT views FROM profileviews WHERE \`identifier\`="${profile}"`))[0];
     var url;
-    if (!views){
+    if (!views) {
         views = 1;
-        url = `https://img.shields.io/badge/Profile%20Views-${views}-${color ?? 'blue'}?style=${style ?? 'for-the-badge'}`;
+        url = `https://img.shields.io/badge/Profile%20Views-${views}-${color ?? "blue"}?style=${style ?? "for-the-badge"}`;
         db.query(`INSERT INTO \`profileviews\` (\`identifier\`, \`views\`) VALUES (${db.getPool().escape(profile)}, '1');`);
     } else {
-        views = views['views'];
-        url = `https://img.shields.io/badge/Profile%20Views-${views + 1}-${color ?? 'blue'}?style=${style ?? 'for-the-badge'}`;
+        views = views["views"];
+        url = `https://img.shields.io/badge/Profile%20Views-${views + 1}-${color ?? "blue"}?style=${style ?? "for-the-badge"}`;
         db.query(`UPDATE \`profileviews\` SET \`views\` = '${views + 1}' WHERE \`profileviews\`.\`identifier\` = ${db.getPool().escape(profile)};`);
     }
-    const img = await fetch(url).then(data => data.text());
+    const img = await fetch(url).then((data) => data.text());
     res.set("Content-Type", "image/svg+xml");
     res.send(img);
 });
 
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
     const url = `https://img.shields.io/badge/Error-Invalid%20Endpoint-red?style=for-the-badge`;
-    const img = await fetch(url).then(data => data.text());
+    const img = await fetch(url).then((data) => data.text());
     res.set("Content-Type", "image/svg+xml");
     res.send(img);
 });
 
-app.listen(process.env.port, () => { console.log(`Running on port ${process.env.port}.`); });
+app.listen(process.env.port, () => {
+    console.log(`Running on port ${process.env.port}.`);
+});
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,28 +1,36 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-	connectionLimit : 100,
-	host: process.env.dbhost,
-	user: process.env.dbuser,
-	password: process.env.dbpass,
-    database: process.env.dbdata
+    connectionLimit: 100,
+    host: process.env.dbhost,
+    user: process.env.dbuser,
+    password: process.env.dbpass,
+    database: process.env.dbdata,
 });
 
-function getPool(){ return pool; }
+function getPool() {
+    return pool;
+}
 
-async function query(sql){
+async function query(sql) {
     var results;
-    await pool.promise().query(sql).then((res, err) => { if (err) throw new Error(err); results = res; });
+    await pool
+        .promise()
+        .query(sql)
+        .then((res, err) => {
+            if (err) throw new Error(err);
+            results = res;
+        });
     return results[0];
 }
 
-async function init(){
-    await query("CREATE TABLE IF NOT EXISTS `profileviews` (`id` INT NOT NULL AUTO_INCREMENT, `identifier` VARCHAR(100) NOT NULL, `views` BIGINT NOT NULL, PRIMARY KEY (`id`));")
+async function init() {
+    await query("CREATE TABLE IF NOT EXISTS `profileviews` (`id` INT NOT NULL AUTO_INCREMENT, `identifier` VARCHAR(100) NOT NULL, `views` BIGINT NOT NULL, PRIMARY KEY (`id`));");
 }
 
 module.exports = {
     query: query,
     getPool: getPool,
-    init: init
-}
+    init: init,
+};
